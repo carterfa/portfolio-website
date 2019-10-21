b247a4fbce99c8834bdbaf2b9faebe28c5d4dfce
Farrah Carter <carterfarraha@gmail.com>
2019-10-20 20:04:31 -0400

    added links to portfolio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function readmoreBtn() {
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("readBtn");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("readBtn");
   
     if (moreText.style.display === "inline") {
       btnText.innerHTML = "More";
@@ -11,13 +11,4 @@ function readmoreBtn() {
     }
   }
 
- function DropDown() {
-    var x = document.getElementById("PortfolioNav");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      document.getElementById("DropBtn").style.backgroundColor = "#006666";
-    } else {
-      x.style.display = "none";
-      document.getElementById("DropBtn").style.backgroundColor = "#999999";
-    }
-  } 
+
